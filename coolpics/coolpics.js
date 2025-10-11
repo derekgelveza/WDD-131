d5bd56f44de0dbcc626197ca6312c6bd24101eae
derekgelveza <gel24002@byui.edu>
2025-10-11 14:41:26 -0600

    meny button disapears and nav appears on larger screen, can click on images for the modal

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,10 +1,35 @@
 const nav = document.querySelector("nav");
     nav.classList.add('hide');
 
-const menuButton = document.querySelector(".menu-botton");
-
+const menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", toggleMenu)
 function toggleMenu(){
     nav.classList.toggle("hide");
 };
 
-menuButton.addEventListener("click", toggleMenu);
+const galleryImages = document.querySelectorAll(".gallery img")
+const dialog = document.querySelector(".image-modal")
+const dialogImg = dialog.querySelector(".dialog-img")
+const closeButton = dialog.querySelector(".close-viewer")
+
+galleryImages.forEach(img => {
+    img.addEventListener("click", () => {
+        dialogImg.src = img.src;
+        dialog.showModal();
+    });
+});
+
+closeButton.addEventListener('click', (event) => {
+    dialog.close();
+});
+
+function handleResize(){
+    const menu = document.querySelector("nav")
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide");
+    } else {
+        menu.classList.add("hide");
+    }
+}
+handleResize();
+window.addEventListener("resize", handleResize);
